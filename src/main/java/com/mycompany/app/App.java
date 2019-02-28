06644e67f972ffb13e550be30a3d96d151f4bf54
Abel Chils <abelcht@outlook.es>
2019-02-28 19:50:26 +0100

    saasfd

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -14,104 +14,80 @@ import java.util.Comparator;
 import java.util.List;
 
 // Definicion de entidades
-class Entity {
-    String value;
-
-    Entity(String value) {
-        this.value = value;
-    }
-}
 
 class Photo {
     Number id;
     String orientation;
-    Number tagsNumber;
+    int tagsNumber;
     ArrayList<String> tagsList = new ArrayList<String>();
 
-    Photo(int id, String orientation, Number tagsNumber, ArrayList<String> tagsList) {
+    Photo(int id, String orientation, int tagsNumber, ArrayList<String> tagsList) {
         this.id = id;
         this.orientation = orientation;
         this.tagsNumber = tagsNumber;
         this.tagsList = tagsList;
     }
-
-    @Override
-    public String toString() {
-        return this.id + this.orientation + this.tagsNumber + this.tagsList;
-    }
 }
 
 class Slide {
-    ArrayList<Number> photoIds = new ArrayList<Number>();
+    private Integer photo1;
+    private Integer photo2;
 
-    Slide(ArrayList<Number> photoIds) {
-        this.photoIds = photoIds;
+    Slide(Integer photo1, Integer photo2) {
+        this.photo1 = photo1;
+        this.photo2 = photo2;
+    }
+
+    @Override
+    public String toString() {
+        String salida = photo1.toString();
+        if (photo2 != null){
+            salida = salida.concat(" " + photo2);
+        }
+        return salida.concat("\n");
     }
 }
 
 public class App {
 
     public static void main(String[] args) throws Exception {
+        String fichero = "a_example";
+
         // Leer Fichero Entrada
-        String fileString = readFileAsString("/Users/cokelas/Documents/my-app/a_example.txt");
+        String fileString = readFileAsString(fichero + ".txt");
         System.out.println("    ---> readFileAsString: ");
         System.out.println(fileString);
 
         // Separar String por Saltos de Línea
         List<String> stringArray = splitString(fileString, "\n");
         System.out.println("    ---> splitStringBySeparator: ");
-        for (String string : stringArray) {
-            System.out.println(string);
-        }
 
-        Number photoNumber = Integer.parseInt(stringArray.get(0));
         ArrayList<Photo> photoList = new ArrayList<Photo>();
+
         int i = 0;
         for (String string : stringArray) {
             if (i != 0) {
                 List<String> listaElementos = splitString(string, " ");
-                photoList.add(new Photo(i-1, listaElementos.get(0), Integer.parseInt(listaElementos.get(1)), new ArrayList<>(listaElementos.subList(2, listaElementos.size()))));
+                photoList.add(new Photo(i - 1, listaElementos.get(0), Integer.parseInt(listaElementos.get(1)), new ArrayList<>(listaElementos.subList(2, listaElementos.size()))));
             }
             i++;
         }
 
-        for(Photo photo : photoList) {
-            System.out.println(photo.toString());
-        }
-        
+        ArrayList<Slide> salida = new ArrayList<>();
 
-        // Creación de Entidades
-        // System.out.println("    ---> splitStringBySeparator (with Type Entity): ");
-        // for (String string : stringArray) {
-        //     Entity entity = new Entity(string);
-        //     System.out.println(entity.toString());
-        // }
-
-        // Filtrar objetos de un array
-        // List<String> filteredArray = filterArrayExample(stringArray);
-        // System.out.println("    ---> filterArrayExample: ");
-        // for (String string : filteredArray) {
-        //     System.out.println(string);
-        // }
-
-        // Concatenar elementos de array con saltos de línea
-        // System.out.println("    ---> joinString: ");
-        // String result = joinString(filteredArray, "\n");
-        // System.out.println(result);
-
-        // Guarda string en fichero
-        // System.out.println("    ---> saveFile: ");
-        // saveFile(result, "resultFile.txt");
-    }
-
-    public static List<String> filterArrayExample(List<String> array) {
-        List<String> result = new ArrayList<>();
-        for (String string : array) {
-            if (!string.contains("foo")) {
-                result.add(string);
+        for (Photo actual_photo : photoList) {
+            if (actual_photo.orientation.equals("H")) {
+                salida.add(new Slide(actual_photo.tagsNumber, null));
             }
         }
-        return result;
+
+        String string_output = salida.size() + "\n";
+
+        for (Slide actual_slice : salida) {
+            string_output = string_output.concat(actual_slice.toString());
+        }
+
+        saveFile(string_output, fichero + ".out");
     }
 
     public static String readFileAsString(String fileName) throws Exception {
@@ -128,9 +104,5 @@ public class App {
 
     public static List<String> splitString(String string, String separator) {
         return Arrays.asList(string.split(separator));
-    }
-
-    public static String joinString(List<String> array, String joiner) {
-        return String.join(joiner, array);
     }
 }
