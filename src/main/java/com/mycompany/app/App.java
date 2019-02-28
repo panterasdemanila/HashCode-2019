7ce41e1b0a0231ea82912c033fd22f23a41ab01e
Jorge Martinez <jmlascorz@hiberus.com>
2019-02-28 19:12:03 +0100

    PHOTO LIST

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,6 +1,6 @@
 // PARA EJECUTAR
 // Instalar maven: https://maven.apache.org/install.html
-// Ejecutar: mvn package && java -cp target/my-app-1.0-SNAPSHOT.jar com.mycompany.app.App
+// Ejecutar: mvn -Dmaven.test.skip=true install && java -cp target/my-app-1.0-SNAPSHOT.jar com.mycompany.app.App
 
 package com.mycompany.app;
 
@@ -9,6 +9,8 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 // Definicion de entidades
@@ -20,38 +22,86 @@ class Entity {
     }
 }
 
+class Photo {
+    Number id;
+    String orientation;
+    Number tagsNumber;
+    ArrayList<String> tagsList = new ArrayList<String>();
+
+    Photo(int id, String orientation, Number tagsNumber, ArrayList<String> tagsList) {
+        this.id = id;
+        this.orientation = orientation;
+        this.tagsNumber = tagsNumber;
+        this.tagsList = tagsList;
+    }
+
+    @Override
+    public String toString() {
+        return this.id + this.orientation + this.tagsNumber + this.tagsList;
+    }
+}
+
+class Slide {
+    ArrayList<Number> photoIds = new ArrayList<Number>();
+
+    Slide(ArrayList<Number> photoIds) {
+        this.photoIds = photoIds;
+    }
+}
+
 public class App {
 
     public static void main(String[] args) throws Exception {
         // Leer Fichero Entrada
-        String fileString = readFileAsString("/Users/cokelas/Documents/my-app/fileName.txt");
+        String fileString = readFileAsString("/Users/cokelas/Documents/my-app/a_example.txt");
         System.out.println("    ---> readFileAsString: ");
         System.out.println(fileString);
+
         // Separar String por Saltos de Línea
         List<String> stringArray = splitString(fileString, "\n");
         System.out.println("    ---> splitStringBySeparator: ");
         for (String string : stringArray) {
             System.out.println(string);
         }
-        // Creación de Entidades
-        System.out.println("    ---> splitStringBySeparator (with Type Entity): ");
+
+        Number photoNumber = Integer.parseInt(stringArray.get(0));
+        ArrayList<Photo> photoList = new ArrayList<Photo>();
+        int i = 0;
         for (String string : stringArray) {
-            Entity entity = new Entity(string);
-            System.out.println(entity.toString());
+            if (i != 0) {
+                List<String> listaElementos = splitString(string, " ");
+                photoList.add(new Photo(i-1, listaElementos.get(0), Integer.parseInt(listaElementos.get(1)), new ArrayList<>(listaElementos.subList(2, listaElementos.size()))));
+            }
+            i++;
         }
+
+        for(Photo photo : photoList) {
+            System.out.println(photo.toString());
+        }
+        
+
+        // Creación de Entidades
+        // System.out.println("    ---> splitStringBySeparator (with Type Entity): ");
+        // for (String string : stringArray) {
+        //     Entity entity = new Entity(string);
+        //     System.out.println(entity.toString());
+        // }
+
         // Filtrar objetos de un array
-        List<String> filteredArray = filterArrayExample(stringArray);
-        System.out.println("    ---> filterArrayExample: ");
-        for (String string : filteredArray) {
-            System.out.println(string);
-        }
+        // List<String> filteredArray = filterArrayExample(stringArray);
+        // System.out.println("    ---> filterArrayExample: ");
+        // for (String string : filteredArray) {
+        //     System.out.println(string);
+        // }
+
         // Concatenar elementos de array con saltos de línea
-        System.out.println("    ---> joinString: ");
-        String result = joinString(filteredArray, "\n");
-        System.out.println(result);
+        // System.out.println("    ---> joinString: ");
+        // String result = joinString(filteredArray, "\n");
+        // System.out.println(result);
+
         // Guarda string en fichero
-        System.out.println("    ---> saveFile: ");
-        saveFile(result, "resultFile.txt");
+        // System.out.println("    ---> saveFile: ");
+        // saveFile(result, "resultFile.txt");
     }
 
     public static List<String> filterArrayExample(List<String> array) {
